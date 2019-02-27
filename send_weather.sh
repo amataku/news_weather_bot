bedc2e047be2a5f3927609462e8bcf71997e0ed3
amataku <amataku13@gmail.com>
2019-02-27 18:39:51 +0900

    rebase

--- send_weather.sh
+++
@@ -1,4 +0,0 @@
-export NEWS_API_KEY="846b52ee5c4c4a1592c3d0a3f92434dd"
-export NEWS_SLACK="TBF9GDTGB/BD3KFUJN5/NSKyYUPALrqWHQ43HTLVGZbC"
-
-/home/amataku/.pyenv/shims/python3 /home/amataku/Project/news_weather_bot/send_weather.py
